901f8bc6cead19c0b5a46788f73ad8d0212baf52
ihan60220 <124228768+ihan60220@users.noreply.github.com>
2023-02-01 22:54:55 -0600

    Add files via upload

--- hedge.py
+++ hedge.py
@@ -4,6 +4,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+Home = input("Enter name of team: ")
+
 driver = webdriver.Chrome()
 
 driver.get("https://www.bovada.lv/sports/basketball")
@@ -12,11 +14,18 @@ while True:
     time.sleep(5)
 
     try:
-        elements = driver.find_elements(By.CLASS_NAME, "bet-price")
-        oddsA = int(elements[2].text)
-        oddsB = int(elements[3].text)
-        print(oddsA, oddsB)
+        names = driver.find_elements(By.CLASS_NAME, "name")
+        indexA = 0
+        for index, name in enumerate(names):
+            if name.text == Home:
+                indexA = index
+        nameA = names[indexA].text
+        nameB = names[indexA + 1].text
+        odds = driver.find_elements(By.CLASS_NAME, "bet-price")
+        oddsA = int(odds[3 * indexA + 2].text)
+        oddsB = int(odds[3 * indexA + 3].text)
+        print(nameA, oddsA, nameB, oddsB)
     except:
-        pass
+        print("could not fetch")
 
 driver.quit()
