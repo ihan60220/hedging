a986ae868a3eff90ca6b797ebd38ee902fc45a15
ihan60220 <124228768+ihan60220@users.noreply.github.com>
2023-02-01 22:09:21 -0600

    Add files via upload

--- hedge.py
+++ hedge.py
@@ -6,14 +6,15 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
-driver.get("https://www.bovada.lv/sports/football")
+driver.get("https://www.bovada.lv/sports/basketball")
 
-time.sleep(5)
+while True:
+    time.sleep(5)
 
-elements = driver.find_elements(By.CLASS_NAME, "bet-price")
+    elements = driver.find_elements(By.CLASS_NAME, "bet-price")
 
-oddsA = int(elements[2].text)
-oddsB = int(elements[3].text)
-print(oddsA, oddsB)
+    oddsA = int(elements[2].text)
+    oddsB = int(elements[3].text)
+    print(oddsA, oddsB)
 
 driver.quit()
