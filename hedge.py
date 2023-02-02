44e7a951f7e143b5a2fbff020b3dbf16658764e3
ihan60220 <124228768+ihan60220@users.noreply.github.com>
2023-02-01 22:18:19 -0600

    Add files via upload

--- hedge.py
+++ hedge.py
@@ -11,10 +11,12 @@ driver.get("https://www.bovada.lv/sports/basketball")
 while True:
     time.sleep(5)
 
-    elements = driver.find_elements(By.CLASS_NAME, "bet-price")
-
-    oddsA = int(elements[2].text)
-    oddsB = int(elements[3].text)
-    print(oddsA, oddsB)
+    try:
+        elements = driver.find_elements(By.CLASS_NAME, "bet-price")
+        oddsA = int(elements[2].text)
+        oddsB = int(elements[3].text)
+        print(oddsA, oddsB)
+    except:
+        pass
 
 driver.quit()
