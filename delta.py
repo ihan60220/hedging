b15430debfc0761bfdc8e0840a0942e82893f470
ihan60220 <124228768+ihan60220@users.noreply.github.com>
2023-02-03 12:59:57 -0600

    Add files via upload

--- delta.py
+++ delta.py
@@ -1,4 +1,4 @@
-# fetching odds from bovada
+# delta hedging applied to sports betting
 
 import time
 import datetime
@@ -15,9 +15,9 @@ driver.get("https://www.bovada.lv/?overlay=login")
 # user should get everything set up before proceeding
 input("Press Enter When Done With Setup: ")
 
-for t in range(10):
-    # loop every five seconds
-    time.sleep(5)
+for t in range(5):
+    # loop every ten seconds
+    time.sleep(10)
     
     # fetch the bet slip
     bet_slip = driver.find_elements(By.CLASS_NAME, "top-line")
@@ -35,5 +35,14 @@ for t in range(10):
     Delta Hedging Algorithm Here
 
     """
+
+    # if determined that bet should be hedged for teamA
+    make_bets = driver.find_elements(By.ID, "default-input--risk")
+    bet_on_A = make_bets[0].send_keys("1")
+    bet_on_B = make_bets[1].send_keys("1")
+    place_bets = driver.find_element(By.CLASS_NAME, "betslip-btn-container")
+    place_bets = place_bets.find_element(By.TAG_NAME, "button")
+    place_bets.click()
+    
 
 driver.quit()
