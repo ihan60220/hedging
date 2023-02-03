7e99b4240cee4e188b1e61cfbe3eb95ac8b4b98e
ihan60220 <124228768+ihan60220@users.noreply.github.com>
2023-02-03 10:49:25 -0600

    Add files via upload

--- hedge.py
+++ hedge.py
@@ -5,13 +5,9 @@ import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-website = input("Type In Sport: ")
 driver = webdriver.Chrome()
 
-try:
-    driver.get("https://www.bovada.lv/sports/" + website.lower())
-except:
-    print("Invalid Sport")
+driver.get("https://www.bovada.lv/?overlay=login")
 
 # get the name of team
 Home = input("Enter name of team: ")
@@ -19,7 +15,6 @@ Home = input("Enter name of team: ")
 # create a list to contain all odds
 container = []
 
-# for the checkname function
 indicator = 0
 
 try:
@@ -63,6 +58,24 @@ try:
             # output the odds with timestamps
             container.append((datetime.datetime.now(), oddsA, oddsB))
             print(container[-1][0], nameA, container[-1][1], nameB, container[-1][2])
+
+            # make bets if meets condition
+            try:
+                buttons = driver.find_elements(By.CLASS_NAME, "bet-btn")
+                buttons[3 * indexA + 2].click()
+            except:
+                print("Bet Not Available.")
+            
+            # wait for bet slip to load
+            time.sleep(5)
+
+            try:
+                risk = driver.find_element(By.ID, "default-input--risk")
+                risk.send_keys("10")
+            except:
+                print("trying to click on input box")
+        
+
 
         except:
             #if something goes wrong while fetching, output error message
