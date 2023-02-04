c9db44abc875a3c110838c60af522eb68ef1fdc3
ihan60220 <124228768+ihan60220@users.noreply.github.com>
2023-02-04 02:09:54 -0600

    Add files via upload

--- delta.py
+++ delta.py
@@ -16,6 +16,8 @@ def isDuplicate(odds, oddslist):
     if len(oddslist) < 2:
         oddslist.append(odds)
     elif odds != oddslist[-1]:
+        oddslist.pop(0)
+        oddslist.append(odds)
         return False
     return True
 
@@ -26,8 +28,6 @@ def derivativefinder(oddslist):
         return True
     elif dydx < 0:
         return False
-    if len(oddslist) > 1:
-        oddslist.pop(0)
 
 def American_to_Decimal(odds):
     if odds > 0:
@@ -40,7 +40,7 @@ def determinestakeB(oddsA, stakeA, oddsB):
 
 def makeABet(stakeB):
     make_bet = driver.find_element(By.XPATH, '//*[@id="default-input--risk"]')
-    make_bet.send_key(str(stakeB))
+    make_bet.send_keys(str(stakeB))
     make_bet = driver.find_element(By.XPATH, '/html/body/bx-site/ng-component/div/sp-sports-ui/div/main/section/div/div/sp-betslip-area/article/sp-betslip/div/div[2]/footer/button')
     make_bet.click()
 
@@ -56,9 +56,8 @@ input("Press Enter When Done With Setup: ")
 
 #global variables for david
 nameA = input("Enter team A name: ")#Can be replaced with a Selenium path later
-oddsA = input("Enter odds for A: ")#Can be replaced with a Selenium path later
-oddsA = int(oddsA)
-stakeA = 1  #Can either be prompted input or detected from Selenium driver
+oddsA = int(input("Enter odds for A: "))#Can be replaced with a Selenium path later
+stakeA = round(float(input("Enter stakeA: ")), 2)  #Can either be prompted input or detected from Selenium driver
 stakeB = 0
 oddsBlist = []
 
@@ -72,20 +71,31 @@ while True:
     nameB = teamB[0].text
     oddsB = int(teamB[1].text)
 
-    if isDuplicate() == True:
+    print(oddsB)
+
+    if isDuplicate(oddsB, oddsBlist) == True:
         continue
 
     stakeB = determinestakeB(oddsA, stakeA, oddsB)
+    print(stakeB)
+    if stakeB > 5:
+        break
+
     TruePayoutB = determine_sidepayout(oddsB, stakeB)
 
     if TruePayoutB - stakeB - stakeA > 0 and derivativefinder(oddsBlist):
         makeABet(stakeB)
         hedgelog = open("closedpositionslog.txt","a")
-        x = datetime.datetime.now()
-        hedgelog.write(nameA + " vs. " + nameB + x.strftime("%c") + "   Total Leveraged: " + str(stakeA + stakeB) + "   Payout:"+ str(TruePayoutB - stakeB - stakeA))
+        currTime = datetime.now()
+        hedgelog.write(nameA + " vs. " + nameB + currTime.strftime("%c") + "   Total Leveraged: " + str(stakeA + stakeB) + "   Payout:"+ str(TruePayoutB - stakeB - stakeA))
         hedgelog.close()
         del oddsBlist[:]
         break
+    else:
+        print(str(TruePayoutB - stakeB - stakeA))
+
+
+time.sleep(60)
 
 driver.quit()
 
