b5d834fc507181f1c3d9f7b52a171055d290ebd3
dar58965 <124225446+dar58965@users.noreply.github.com>
2023-02-17 00:11:38 -0600

    Update Bovada login test.py

--- Bovada login test.py
+++ Bovada login test.py
@@ -11,10 +11,9 @@ driver = webdriver.Chrome(options=options)
 
 driver.get("https://www.bovada.lv/?overlay=login")
 
-time.sleep(5)
+time.sleep(1.5)
 fill_out = driver.find_element(By.XPATH, '//*[@id="email"]')
 fill_out.send_keys("dar58965@gmail.com")
-time.sleep(.75)
 fill_out = driver.find_element(By.XPATH, '//*[@id="login-password"]')
 fill_out.send_keys("Lpgg3586Rnbx5455")
 time.sleep(.75)
